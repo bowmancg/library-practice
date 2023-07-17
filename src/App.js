0cc0201bd361d7b4fb3fec1f9953022a72acd6c8
Chandler Bowman <doctorgrant99@gmail.com>
2023-07-17 12:51:11 -0600

    added demo list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
-import { useState, useCallback } from 'react';
+import { useState, useCallback, Fragment, useMemo } from 'react';
 import Button from './components/UI/Button/Button';
 import DemoOutput from './components/UI/Button/Demo/DemoOutput';
+import DemoList from './components/UI/Button/Demo/DemoList';
 
 function App() {
+  const [listTitle, setListTitle] = useState('My List')
+
   const [showPar, setShowPar] = useState(false)
   const [allowToggle, setAllowToggle] = useState(false)
 
   console.log('App running.')
 
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title')
+  }, [])
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], [])
+
   const toggleParHandler = useCallback(() => {
-    if (allowToggle) {      
+    if (allowToggle) {
       setShowPar((prevShowPar) => !prevShowPar)
     }
   }, [allowToggle])
@@ -20,12 +29,18 @@ function App() {
   }
 
   return (
-    <div className="app">
-      <h1>Welcome.</h1>
-      <DemoOutput show={showPar} />
-      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
-      <Button onClick={toggleParHandler}>Toggle Paragraph.</Button>
-    </div>
+    <Fragment>
+      <div className="app">
+        <h1>Welcome.</h1>
+        <DemoOutput show={showPar} />
+        <Button onClick={allowToggleHandler}>Allow Toggling</Button>
+        <Button onClick={toggleParHandler}>Toggle Paragraph.</Button>
+      </div>
+      <div className='app'>
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
+      </div>
+    </Fragment>
   );
 }
 
