eaf62308c4f0f63c95700b8d108a62bd442207d5
Chandler Bowman <doctorgrant99@gmail.com>
2023-07-17 11:37:40 -0600

    added use callback

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import Button from './components/UI/Button/Button';
 import DemoOutput from './components/UI/Button/Demo/DemoOutput';
 
 function App() {
   const [showPar, setShowPar] = useState(false)
+  const [allowToggle, setAllowToggle] = useState(false)
 
   console.log('App running.')
 
-  const toggleParHandler = () => {
-    setShowPar((prevShowPar) => !prevShowPar)
+  const toggleParHandler = useCallback(() => {
+    if (allowToggle) {      
+      setShowPar((prevShowPar) => !prevShowPar)
+    }
+  }, [allowToggle])
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true)
   }
 
   return (
     <div className="app">
       <h1>Welcome.</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showPar} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParHandler}>Toggle Paragraph.</Button>
     </div>
   );
